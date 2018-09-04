d4b30ce6c8a720bd42ac9a455ee8c7fa4b27c00d
Carlos Lopez <clopezr1@uncc.edu>
2018-09-04 21:24:54 +0000

    upto RPS

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,41 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+  end
+  def name
+    @name
+  end
+  def name=(newName)
+    @name = newName
+  end
+  def calories
+    @calories
+  end
+  def calories=(newCalories)
+    @calories = newCalories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @name = "#{@flavor} jelly bean"
+    @calories = 5
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(newflavor)
+    @flavor = newflavor
+  end
+  def delicious?
+    return true unless @flavor.downcase == "licorice"
   end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,13 +1,17 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    p = self.downcase.gsub(/[^a-z0-9]+/i, '')
+    p == p.reverse 
   end
   def count_words
-    # your code here
+    words = Hash.new(0)
+    p = self.downcase.gsub(/[^a-z0-9 ]+/i, '')
+    p.split.each {|x| words[x] = words[x] + 1}
+    words
   end
-  def anagram_groups
-    # your code here
-  end
+  def anagram_groups 
+    p = self.downcase.split.group_by {|x| x.chars.sort}.values
+  end 
 end
 
 # make all the above functions available as instance methods on Strings:
@@ -15,3 +19,4 @@ end
 class String
   include FunWithStrings
 end
+		
