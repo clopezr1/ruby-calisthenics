ca7fcc1b8485d21a8326130bf08123289ff77114
Carlos Lopez <clopezr1@uncc.edu>
2018-09-11 21:51:36 +0000

    Calisthenics Final

--- lib/cartesian_product.rb
+++ lib/cartesian_product.rb
@@ -1,4 +1,16 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+  
+  def initialize(seq1, seq2)
+    @seq1 = seq1
+    @seq2 = seq2
+  end
+  
+  def each
+    @seq1.each { |a| 
+      @seq2.each { |b|
+          yield [a,b]
+      }
+    }
+  end
 end
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,25 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
+    p1 = player1[1]
+    p2 = player2[1]
     
+    case p1 + p2 
+      when 'PR','SP','RS','RR','SS','PP'
+        then player1
+      when 'RP','PS','SR'
+        then player2
+      else 
+        raise NoSuchStrategyError.new ("Strategy must be one of R,P,S")
+      end   
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if  tournament[0][0].kind_of?(String)
+      winner(tournament[0],tournament[1])
+    else
+      winner(tournament_winner(tournament[0]),tournament_winner(tournament[1]))
+    end
   end
 
 end
